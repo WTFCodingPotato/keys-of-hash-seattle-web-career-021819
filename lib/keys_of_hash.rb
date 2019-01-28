60023d5fca7204395f276902d337efd2d95267d6
Joshua M Loran <joshua.loran@gmail.com>
2019-01-28 17:38:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
   def keys_of(*arguments)
     array = []
+    arguments.each do |x|
+      self.each do |y,z|
+        if x == z
+          array << y
+        end
+      end
+    end
+    array
   end
 end
